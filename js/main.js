875c91852fa3711d0b41fa9ec197e74275169095
ellvenro <lidiya.ellie@yandex.ru>
2021-07-31 17:40:40 +0300

    Редактирование двух блоков в основной части страницы

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function MouseOver(el)
+{
+  el.setAttribute(src, "url('css/images/1pr_2.png')");
+}
+
+// function MouseOut(el)
+// {
+//   el.style.backgroundImage = "url('css/images/1pr_1.png')";
+// }
