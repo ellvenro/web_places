a2f01cf4df401b7ab214fe188b15c4e3136c8e6b
ellvenro <lidiya.ellie@yandex.ru>
2021-08-02 17:13:32 +0300

    Создание слайдера с автоматическим переключением

--- js/main.js
+++ js/main.js
@@ -1,3 +1,57 @@
-// if (document.documentElement.clientWidth < 960) {
-//   document.getElementById("stylesheet").href="css/style_header_min.css";
-// }
+const prev = document.getElementById('btn_prev'),
+      next = document.getElementById('btn_next'),
+      // slides = document.getElementsByClass('slide'),
+      slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+// function activeSlide(n) {
+const activeSlide = n => {
+  for(slide of slides) {
+    slide.classList.remove('active');
+  }
+  slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+  for(dot of dots) {
+    dot.classList.remove('active');
+  }
+  dots[n].classList.add('active');
+}
+
+const active = ind => {
+  activeSlide(ind);
+  activeDot(ind);
+}
+
+// function mextSlide() {
+const nextSlide = () => {
+  if(index == slides.length - 1) {
+    index = 0;
+  } else {
+    index++;
+  }
+  active(index);
+}
+
+const prevSlide = () => {
+  if(index == 0) {
+    index = slides.length - 1;
+  } else {
+    index--;
+  }
+  active(index);
+}
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index = indexDot;
+    active(index);
+  })
+})
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+setInterval(nextSlide, 2500);
